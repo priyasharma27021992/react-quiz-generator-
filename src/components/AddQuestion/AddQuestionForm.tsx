9dbb2e816ad09cc20b88ab77b45dd6581f3e4538
Priya.Sharma <priyasharma2702@gmail.com>
2025-01-15 22:35:03 +0530

    some code added for add question form

--- src/components/AddQuestion/AddQuestionForm.tsx
+++ src/components/AddQuestion/AddQuestionForm.tsx
@@ -1,1 +1,10 @@
-const AddQuestionForm = () => {};
+const AddQuestionForm = () => {
+  return (
+    <form action="">
+      <input name="question" type="text" />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export { AddQuestionForm };
