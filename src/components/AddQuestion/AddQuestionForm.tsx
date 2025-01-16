5e0e03f226f4d2f60782cb49e7fd30c4c15ffb42
Priya.Sharma <priyasharma2702@gmail.com>
2025-01-16 23:47:22 +0530

    add question form added

--- src/components/AddQuestion/AddQuestionForm.tsx
+++ src/components/AddQuestion/AddQuestionForm.tsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
+
 const AddQuestionForm = () => {
+  const inputArr = [
+    {
+      type: "text",
+      id: 1,
+      value: "",
+    },
+  ];
+
+  const [arr, setArr] = useState(inputArr);
+
+  const addInput = () => {
+    setArr((s) => {
+      return [
+        ...s,
+        {
+          type: "text",
+          value: "",
+        },
+      ];
+    });
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    const index = e.target.id;
+    setArr((s) => {
+      const newArr = s.slice();
+      newArr[index].value = e.target.value;
+
+      return newArr;
+    });
+  };
   return (
     <form action="">
-      <input name="question" type="text" />
-      <button>Add</button>
+      <input name="question" type="text" required />
+      <div>
+        <button onClick={addInput}></button>
+        {arr.map((item, i) => {
+          return (
+            <input
+              onChange={handleChange}
+              value={item.value}
+              key={i}
+              type={item.type}
+            />
+          );
+        })}
+      </div>
+      <button type="submit">Add Question</button>
     </form>
   );
 };
--- src/components/QuizContainer/QuizContainer.tsx
+++ src/components/QuizContainer/QuizContainer.tsx
@@ -1,8 +1,10 @@
+import { AddQuestionForm } from "../AddQuestion/AddQuestionForm";
 import { QuizQuestion } from "./QuizQuestion/QuizQuestion";
 
 const QuizContainer = () => {
   return (
     <div className="max-w-[700px] mx-auto my-20">
+      <AddQuestionForm />
       <h2 className="mb-3 font-bold text-xl">
         Well, so here is the quiz, select the right answer and click to next
         question after that?
