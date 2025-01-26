91a77c3b152812a2cec36dcb4adb1439f902da94
Priya.Sharma <priyasharma2702@gmail.com>
2025-01-26 21:45:22 +0530

    added on blur on modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useCallback, useEffect, useRef } from "react";
 
 type ModalProps = {
   openModal: boolean;
@@ -6,9 +6,28 @@ type ModalProps = {
   children?: ReactElement | ReactNode;
 };
 const Modal = ({ openModal, closeModal, children }: ModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
   const handleCloseModal = () => {
     closeModal();
   };
+
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    },
+    [closeModal]
+  );
+
+  useEffect(() => {
+    if (openModal) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside, openModal]);
 
   if (openModal !== true) return null;
   return (
@@ -16,7 +35,10 @@ const Modal = ({ openModal, closeModal, children }: ModalProps) => {
       className="fixed inset-0 bg-black flex justify-center items-center bg-opacity-20 backdrop-blur-sm"
       role="dialog"
     >
-      <div className="relative p-4 w-full max-w-2xl max-h-full bg-white shadow-inner border-emerald-600 rounded-lg">
+      <div
+        className="relative p-4 w-full max-w-2xl max-h-full bg-white shadow-inner border-emerald-600 rounded-lg"
+        ref={modalRef}
+      >
         <div className="flex justify-end py-2">
           <button onClick={handleCloseModal} className="">
             X
