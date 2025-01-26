d925bc07bbb9691574a87697ab56937dca232789
Priya.Sharma <priyasharma2702@gmail.com>
2025-01-26 21:46:18 +0530

    added types of questions

--- src/contexts/QuestionContext.tsx
+++ src/contexts/QuestionContext.tsx
@@ -1,10 +1,11 @@
 import { createContext, useState } from "react";
 import { QUESTIONS_ARRAY } from "../utils/common";
+import { questionType } from "../utils/types";
 
 const QuestionContext = createContext();
 
 const QuestionProvider = ({ children }) => {
-  const [questions, setQuestions] = useState(QUESTIONS_ARRAY);
+  const [questions, setQuestions] = useState<questionType[]>(QUESTIONS_ARRAY);
   return (
     <QuestionContext.Provider value={{ questions, setQuestions }}>
       {children}
